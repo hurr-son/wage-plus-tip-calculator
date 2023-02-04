f67a0f9b9b944e3dc7f7d41278dfd951f1d45d33
hurr-son <hprewett05@gmail.com>
2023-02-04 14:06:55 -0600

    wage displays on screen and error throw if NaN

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-const wageInput = document.getElementById("wageInput");
-const tipInput = document.getElementById("tipInput");
-const hoursInput = document.getElementById("hoursInput");
-const calcBtn = document.getElementById("calcBtn");
-const displayWage = document.querySelector(".display-wage");
+(function () {
+  const wageInput = document.getElementById("wageInput");
+  const tipInput = document.getElementById("tipInput");
+  const hoursInput = document.getElementById("hoursInput");
+  const calcBtn = document.getElementById("calcBtn");
+  const displayWage = document.querySelector(".display-wage");
+  let realWage = "";
 
+  const moneyFormatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
 
+  function calculateRealWage(wage, tips, hours) {
+    return moneyFormatter.format((wage * hours + tips) / hours);
+  }
 
-const formatter =
-new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
+  calcBtn.addEventListener("click", function () {
+    let wage = parseFloat(wageInput.value);
+    let tips = parseFloat(tipInput.value);
+    let hours = parseFloat(hoursInput.value);
+    wageInput.value = "";
+    tipInput.value = "";
+    hoursInput.value = "";
 
-let wage = 14.50;
-let tips = 20;
-let hours = 7.25;
-
-
-
-function calculateRealWage(wage, tips, hours) {
-     let realWage = formatter.format(((wage * hours) + tips) / hours);
-     return realWage
-}
-
-console.log(calculateRealWage(wage, tips, hours))
+    if (Number.isNaN(wage) || Number.isNaN(tips) || Number.isNaN(hours)) {
+      alert("Input must be a number value");
+      return;
+    } else {
+      realWage = calculateRealWage(wage, tips, hours);
+      displayWage.innerHTML = `${realWage}/hr.`;
+    }
+  });
+})();
