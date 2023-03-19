3af36740888bbdc6a14d9f15af0eeed06ecc8e3f
hurr-son <hprewett05@gmail.com>
2023-03-18 21:11:16 -0500

    added total earnings calculation and display

--- script.js
+++ script.js
@@ -4,7 +4,11 @@
   const hoursInput = document.getElementById("hoursInput");
   const calcBtn = document.getElementById("calcBtn");
   const displayWage = document.querySelector(".display-wage");
+  const displayTotalEarnings = document.querySelector(
+    ".display-total-earnings"
+  );
   let realWage = "";
+  let totalEarnings = "";
 
   const moneyFormatter = new Intl.NumberFormat("en-US", {
     style: "currency",
@@ -12,23 +16,28 @@
   });
 
   function calculateRealWage(wage, tips, hours) {
-    return moneyFormatter.format((wage * hours + tips) / hours);
+    return (wage * hours + tips) / hours;
+  }
+
+  function calculateTotalEarnings(wage, tips, hours) {
+    return wage * hours + tips;
   }
 
   calcBtn.addEventListener("click", function () {
     let wage = parseFloat(wageInput.value);
     let tips = parseFloat(tipInput.value);
     let hours = parseFloat(hoursInput.value);
-    wageInput.value = "";
-    tipInput.value = "";
-    hoursInput.value = "";
 
     if (Number.isNaN(wage) || Number.isNaN(tips) || Number.isNaN(hours)) {
       alert("Input must be a number value");
       return;
     } else {
-      realWage = calculateRealWage(wage, tips, hours);
+      realWage = moneyFormatter.format(calculateRealWage(wage, tips, hours));
+      totalEarnings = moneyFormatter.format(
+        calculateTotalEarnings(wage, tips, hours)
+      );
       displayWage.innerHTML = `${realWage}/hr.`;
+      displayTotalEarnings.innerHTML = `Total Earnings: ${totalEarnings}`;
     }
   });
 })();
